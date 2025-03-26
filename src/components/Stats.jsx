d140db5516c819f741944d049d843bbf5f257949
edward <edwardlfish@gmail.com>
2025-03-26 14:08:33 +0200

    Add TwitterContext

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,12 +1,16 @@
 import Avatar from "./Avatar.jsx";
+import {useContext} from "react";
+import {TwitterContext} from "../utils/context.js";
 
-const Stats = ({user, stats}) => {
+const Stats = () => {
+    const {user, stats} = useContext(TwitterContext);
+
     return (
         <div className={`user-stats`}>
-           <div>
-               <Avatar user={user}/>
-               {user.name}
-           </div>
+            <div>
+                <Avatar user={user}/>
+                {user.name}
+            </div>
             <div className={`stats`}>
                 <div>Followers: {stats.followers}</div>
                 <div>Following: {stats.following}</div>
