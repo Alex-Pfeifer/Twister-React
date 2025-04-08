81dca11c9bd7b9389a68af75613a745e23bea4c6
Alex-Pfeifer <pfeifer.alex@hotmail.de>
2025-04-08 11:04:22 +0200

    Reducer + rootReducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,13 +1,12 @@
 import {CHANGE_AVATAR, CHANGE_NAME} from "../actions/userAction.js";
 
-/*
-type User = {
-name: string,
-avatar: string
-}
-*/
+const initialState = {
+    name: 'Monster',
+    avatar: 'https://gravatar.com/avatar/000?d=monsterid'
 
-export const userReducer = (user, action) => {
+}
+
+export const userReducer = (user = initialState, action) => {
     switch (action.type) {
         case CHANGE_AVATAR:
             return {...user, avatar: action.payload || user.avatar};
