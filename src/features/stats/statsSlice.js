17af17b7e831ef9c8098cf5aa79b40b0eb32821d
Alex-Pfeifer <pfeifer.alex@hotmail.de>
2025-04-08 12:47:55 +0200

    statsSlice

---
+++ src/features/stats/statsSlice.js
@@ -0,0 +1,20 @@
+
+import {createSlice} from "@reduxjs/toolkit";
+
+
+const statsSlice = createSlice({
+    name: 'stats',
+    initialState: {
+        followers: 0,
+        following: 0
+    },
+    reducers: {
+        changeStats: (state, action) => {
+            const res = state[action.type.statsType] + action.payload.sum;
+            state[action.payload.statsType] = res < 0 ? 0 : res;
+        }
+    }
+});
+
+export default statsSlice.reducer;
+export const {changeStats} = statsSlice.actions;
